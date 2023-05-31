1972016d26e2b2d66ffaafb9eecb2d7199fe7724
Ayush Singh <akhileshayush.2016@gmail.com>
2023-05-31 22:01:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your code here
+.city{
+	max-width: 600px;
+margin: 10px;
+padding: 15px;
+border-left: 8px solid green;
+background-color: #f8fffe;
+font-weight: bold;
+	text-decoration:none;
+	color:black;
+
+}
